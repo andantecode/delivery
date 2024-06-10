0ab62f4643133afed657e01c8762f34060c20579
andantecode <kb721a@naver.com>
2024-06-10 14:12:50 +0900

    #2-1] User Domain  1. UserEntity  2. User Service Logic

---
+++ api/src/main/java/org/delivery/api/domain/user/controller/model/UserRegisterRequest.java
@@ -0,0 +1,27 @@
+package org.delivery.api.domain.user.controller.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRegisterRequest {
+
+    @NotBlank
+    private String name;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String address;
+}
--- db/src/main/java/org/delivery/db/acoount/AccountRepository.java
+++
@@ -1,6 +0,0 @@
-package org.delivery.db.acoount;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<AccountEntity, Long> {
-}
